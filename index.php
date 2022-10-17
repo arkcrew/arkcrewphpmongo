b64af323b86b043e774b938e0db3793f43ef636c
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-10-17 12:09:39 +0500

    made changes

--- index.php
+++ index.php
@@ -9,52 +9,47 @@
     <title>Anosha Javed</title>
   </head>
   <body>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="bootstrap.bundle.min.js"></script>
-    <script src="/validation.js"></script>
-       <div class="container p-5" >
-        <div class="row">
-            <div id="bio" class="col-12 col-lg-12 text-center">
-                <img src="user.jpg" class="img-fluid" width="80" height="80">
-                <h1>Registration Form</h1>
-            </div>
-            </div>
-            <div class="justify-content-center">
-            <div class="col-12 col-lg-6 mx-auto border p-4">
-                <form id="theForm" name="myForm" action="config.php" method="POST" >
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Name</label>
-                        <input type="text" class="form-control" id="exampleInputName1" name="fname" placeholder="Enter name">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Last Name</label>
-                        <input type="text" class="form-control" id="exampleInputName1" name="lname"  placeholder="Enter last name">
-                      </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" name="femail" aria-describedby="emailHelp" placeholder="Enter email">
-                      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1" name="fpassword" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Confirm Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" name="fcpassword" placeholder="Confirm Password">
-                      </div>
-                      <div class="row justify-content-center">
-                    <button type="submit" onclick="return validateForm()" name="save" class="btn btn-info justify-content-center">Submit</button>
-                </div>
-                  </form>
-            </div>
-        </div>
-    </div>
-    </div>
-    <script type="text/javascript">
-      function validateForm() {
+  <div class="container p-5" >
+  <div class="row">
+  <div id="bio" class="col-12 col-lg-12 text-center">
+  <img src="user.jpg" class="img-fluid" width="80" height="80">
+  <h1>Registration Form</h1>
+  </div>
+  </div>
+  <div class="justify-content-center">
+  <div class="col-12 col-lg-6 mx-auto border p-4">
+  <form id="theForm" name="myForm" action="config.php" method="POST" >
+  <div class="form-group">
+  <label for="exampleInputEmail1">Name</label>
+  <input type="text" class="form-control" id="exampleInputName1" name="fname" placeholder="Enter name">
+  </div>
+  <div class="form-group">
+  <label for="exampleInputEmail1">Last Name</label>
+  <input type="text" class="form-control" id="exampleInputName1" name="lname"  placeholder="Enter last name">
+  </div>
+  <div class="form-group">
+  <label for="exampleInputEmail1">Email address</label>
+  <input type="email" class="form-control" id="exampleInputEmail1" name="femail" aria-describedby="emailHelp" placeholder="Enter email">
+  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+  <label for="exampleInputPassword1">Password</label>
+  <input type="password" class="form-control" id="exampleInputPassword1" name="fpassword" placeholder="Password">
+  </div>
+  <div class="form-group">
+  <label for="exampleInputPassword1">Confirm Password</label>
+  <input type="password" class="form-control" id="exampleInputPassword1" name="fcpassword" placeholder="Confirm Password">
+  </div>
+  <div class="row justify-content-center">
+  <button type="submit" onclick="return validateForm()" name="save" class="btn btn-info justify-content-center">Submit</button>
+  </div>
+  </form>
+  </div>
+  </div>
+  </div>
+  </div>
+  <script type="text/javascript">
+    function validateForm() {
            let name = document.forms["myForm"]["fname"].value;
            let lname = document.forms["myForm"]["lname"].value;
            let email = document.forms["myForm"]["femail"].value;
@@ -89,5 +84,10 @@
        }
  
     </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="bootstrap.bundle.min.js"></script>
+    <script src="/validation.js"></script>
   </body>
 </html>
